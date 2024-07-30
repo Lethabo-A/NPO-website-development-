fa16186a465b93c46c443f1f8949e0fb0d4cd80a
Lethabo-A <lethaboanyanwu2@gmail.com>
2024-07-30 14:47:54 +0200

    created the database and connected to it

--- Form1.cs
+++ Form1.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Runtime.Remoting.Contexts;
+using System.Xml.Linq;
 
 namespace CMPG223_group20_0_
 {
     public partial class frmLogin : Form
     {
+        string connection = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\StaffDatabase.mdf;Integrated Security=True;Connect Timeout=30";
         public frmLogin()
         {
             InitializeComponent();
@@ -19,7 +23,14 @@ namespace CMPG223_group20_0_
 
         private void frmLogin_Load(object sender, EventArgs e)
         {
-
+            try
+            {
+                SqlConnection con = new SqlConnection(connection);
+            }
+            catch
+            {
+                MessageBox.Show("something doesnt work lol");
+            }
         }
     }
 }
