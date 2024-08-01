0620f02091885901cf8061c30b77d9c3b3a83d51
Lethabo-A <lethaboanyanwu2@gmail.com>
2024-08-01 21:55:42 +0200

    changes
    changes

---
+++ CMPG223_project/WebForm1.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CMPG223_project.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Log in</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblHeading" runat="server" Text="Login"></asp:Label>
+        </div>
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="user number:  "></asp:Label><asp:TextBox ID="txtUserNumber" runat="server"></asp:TextBox>
+        </div>
+    </form>
+</body>
+</html>
---
+++ CMPG223_project/WebForm1.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.UI;
+using System.Data.SqlClient;
+
+namespace CMPG223_project
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        // Connection string using |DataDirectory| for relative path
+        string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\OrganizationDatabase.mdf;Integrated Security=True;Connect Timeout=30";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    
+                }
+            }
+            catch (SqlException ex)
+            {
+                ClientScript.RegisterStartupScript(this.GetType(), "alert", $"alert('Database connection failed: {ex.Message}');", true);
+            }
+        }
+    }
+}
