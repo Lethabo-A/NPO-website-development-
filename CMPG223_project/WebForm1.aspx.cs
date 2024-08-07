5b2c9cc7ab85151ea11942ae44057cfafd298a67
Lethabo-A <lethaboanyanwu2@gmail.com>
2024-08-07 16:41:29 +0200

    login page
    C# and SQL for the login page but not completed

--- CMPG223_project/WebForm1.aspx.cs
+++ CMPG223_project/WebForm1.aspx.cs
@@ -2,37 +2,56 @@
 using System.Web.UI;
 using System.Data.SqlClient;
 using System.Data;
+using System.Xml.Linq;
 
 namespace CMPG223_project
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
-        string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\OrganizationDatabase.mdf;Integrated Security=True;Connect Timeout=30";
         SqlConnection connection = new SqlConnection();
         SqlCommand command = new SqlCommand();
         SqlDataAdapter adapter = new SqlDataAdapter();
         DataTable dt = new DataTable();
         public string chosenLanguage;
-        public string username, password, sqlcommand;
+        public string usernumber, password, sqlcommand, connectionString;
 
         protected void Page_Load(object sender, EventArgs e)
         {
-           
+            connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\OrganizationDatabase.mdf;Integrated Security=True;Connect Timeout=30";
+            try
+            {
+                connection = new SqlConnection(connectionString);
+                connection.Open();
+            }
+            catch (SqlException ex)
+            {
+                ShowAlert("Connection error: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
         }
 
         protected void btnLogin_Click(object sender, EventArgs e)
         {
-            username = txtUserNumber.Text;
+            usernumber = txtUserNumber.Text;
             password = txtPassword.Text;
 
             try
             {
-                // SQL login code here
-
-                if (password == "password" && username == "lethabo")
+                connection.Open();
+                sqlcommand = @"SELECT * FROM Staff where IDNumber ='" + txtUserNumber.Text + "' AND password = '" + txtPassword.Text + "'";
+                adapter = new SqlDataAdapter(sqlcommand, connection);
+                adapter.Fill(dt);
+                if (dt.Rows.Count > 0)
                 {
-                    // Add name of next page in inverted commas
+                    usernumber = txtUserNumber.Text;
+                    password = txtPassword.Text;
+
                     Response.Redirect("Financial_Page_Form.aspx");
+                    password = "";
+                    usernumber = "";
                 }
                 else
                 {
@@ -41,12 +60,24 @@ namespace CMPG223_project
                     txtUserNumber.Text = "";
                     txtUserNumber.Focus();
                 }
+                connection.Close();
 
             }
             catch (SqlException ex)
             {
-                
+                ShowAlert("Connection error: " + ex.Message);
+
             }
+            catch(Exception ex)
+            {
+                ShowAlert("error" + ex.Message);
+            }
+        }
+
+        private void ShowAlert(string message)
+        {
+            string script = $"<script type=\"text/javascript\">alert('{message}');</script>";
+            ClientScript.RegisterStartupScript(this.GetType(), "alert", script);
         }
     }
 }
