b8c441ea5ba853331bee0e011eb0719d599acd03
Lethabo-A <lethaboanyanwu2@gmail.com>
2024-08-13 23:45:32 +0200

    login page redone

--- CMPG223_project/WebForm1.aspx.cs
+++ CMPG223_project/WebForm1.aspx.cs
@@ -1,82 +1,83 @@
 ﻿using System;
 using System.Web.UI;
 using System.Data.SqlClient;
+using System.Data;
 
 namespace CMPG223_project
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
-        string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\OrganizationDatabase.mdf;Integrated Security=True;Connect Timeout=30";
+        SqlConnection connection = new SqlConnection();
+        SqlCommand command = new SqlCommand();
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        DataTable dt = new DataTable();
+        public string chosenLanguage;
+        public string usernumber, password, sqlcommand, connectionString;
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack)
+            //there has been changes made to the connection string to allow it to be used on all our computers please dont change it and please use this connection string when tring to connect on your pc
+            connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\OrganizationDatabase.mdf;Integrated Security=True;Connect Timeout=30";
+            try
             {
+                connection = new SqlConnection(connectionString);
+                connection.Open();
+            }
+            catch (SqlException ex)
+            {
+                ShowAlert("Connection error: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
             }
         }
 
         protected void btnLogin_Click(object sender, EventArgs e)
         {
-            string userNumber = txtUserNumber.Text.Trim();
-            string password = txtPassword.Text.Trim();
-            
-
-            try {
-                SqlConnection connection = new SqlConnection(connectionString);
-                //SQL login code here
-
-                connection.Open();
-                SqlCommand command;
-                SqlDataReader reader;
-                String sql,database_password = "", database_username = "";
-                //int database_username;
-                sql = "select StaffNumber,password from Staff";
-                command = new SqlCommand(sql, connection);
-                reader = command.ExecuteReader();
-                while (reader.Read())
-                {
-                    //database_username = database_username + reader.GetValue(0);
-                    //database_password = database_password + reader.GetValue(5);
-                    database_username = reader["StaffNumber"].ToString();
-                    database_password = reader["password"].ToString();
-
-                    if (password == database_password && userNumber == database_username)
-                {
-                    //add name of next page in inverted commas
-                    Response.Redirect("Financial_Page_Form.aspx");
-                    
-                }
-                else if (password == null)
-                {
-                    //ADD ERROR MESSAGE LBL AND CODE
-                    
-                }
-                    
-                }
-
-                
-
-                
-
-                if (password == database_password && userNumber == database_username)
-                {
-                    //add name of next page in inverted commas
-                    Response.Redirect("Financial_Page_Form.aspx");
-                    
-                }
-                else if (password == null)
-                {
-                    //ADD ERROR MESSAGE LBL AND CODE
-                    
-                }
-
-                connection.Close();
-
-            }
-            catch (SqlException ex)
+            usernumber = txtUserNumber.Text;
+            password = txtPassword.Text;
+            connection.Open();
+            try
             {
                 
+                sqlcommand = @"SELECT * FROM Staff where IDNumber ='" + txtUserNumber.Text + "' AND password = '" + txtPassword.Text + "'";
+                adapter = new SqlDataAdapter(sqlcommand, connection);
+                adapter.Fill(dt);
+                if (dt.Rows.Count > 0)
+                {
+                    usernumber = txtUserNumber.Text;
+                    password = txtPassword.Text;
+                    Response.Redirect("Financial_Page_Form.aspx");
+                    password = "";
+                    usernumber = "";
+                }
+                else
+                {
+                    txtUserNumber.Text = "";
+                    txtUserNumber.Focus();
+                }
+                connection.Close();
             }
+            catch(SqlException ex)
+            {
+                ShowAlert("Connection error: " + ex.Message);
+
+            }
+            catch (Exception ex)
+            {
+                ShowAlert("error" + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+        }
+        private void ShowAlert(string message)
+        {
+            string script = $"<script type=\"text/javascript\">alert('{message}');</script>";
+            ClientScript.RegisterStartupScript(this.GetType(), "alert", script);
         }
     }
 }
