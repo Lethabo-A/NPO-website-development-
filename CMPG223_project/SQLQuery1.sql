a0a9618e35f78c0230ef5a0e1369a23de6c235b1
Gibson-Mlambo <41027892@mynwu.ac.za>
2024-09-23 23:27:21 +0200

    added the table

---
+++ CMPG223_project/SQLQuery1.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE patients (
+    ID INT PRIMARY KEY,
+    name VARCHAR(100),
+    surname VARCHAR(100),
+    age INT,
+    gender VARCHAR(10),
+    allergies VARCHAR(MAX),
+    prescriptions VARCHAR(MAX),
+    current_diagnosis VARCHAR(MAX),
+    past_medical_history VARCHAR(MAX),
+    past_surgical_history VARCHAR(MAX),
+    vaccine_status VARCHAR(MAX),
+    pregnancy_history VARCHAR(MAX),
+    social_history VARCHAR(MAX)
+);
