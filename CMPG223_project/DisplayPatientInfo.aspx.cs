9f828628d5e09d6ab56ca4f57a843a56a5a0f13e
Malique-Edward Mampe <135213323+Malique-Edward@users.noreply.github.com>
2024-09-23 17:30:48 +0200

    Update DisplayPatientInfo.aspx.cs

--- CMPG223_project/DisplayPatientInfo.aspx.cs
+++ CMPG223_project/DisplayPatientInfo.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Data;
@@ -10,10 +10,11 @@ using System.Net.NetworkInformation;
 using System.Configuration;
 using System.Windows;
 
-namespace WebApplication1
+namespace CMPG223_project
 {
     public partial class DisplayPatientInfo : System.Web.UI.Page
     {
+        //variables and verification
         string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\User\Downloads\223 doctor\WebApplication1\App_Data\PatientsInfo.mdf"";Integrated Security=True";
         String sql = "";
         SqlConnection conn;
@@ -24,10 +25,14 @@ namespace WebApplication1
         SqlDataReader dr;
         string pID;
         protected void Page_Load(object sender, EventArgs e)
-        {
+        {//establish cookeis and open connection
             cookie = Request.Cookies["Info"];
-            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString); ;
-            conn.Open();
+            try
+            {
+                conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString); ;
+                conn.Open();
+            }
+            catch (SqlException ex) { MessageBox.Show(ex.Message); }
             pID = cookie["ID"];
             establish();
            
@@ -46,25 +51,35 @@ namespace WebApplication1
         {
             try
             {
+                //update the relevant patients information with the new diagnosis the doctor can come up with
                     conn.Open();
-            
                     string index = listCat.SelectedItem.Value;
                     string updatedValue = txtChanges.Text;
-                    string updateQuery = "UPDATE [patients] SET "+index+" = @update WHERE ID = @id";
-                    cmd = new SqlCommand(updateQuery , conn);
+                if (listCat.SelectedIndex > 0&&updatedValue!="")
+                {
+                    string updateQuery = "UPDATE [patients] SET " + index + " = @update WHERE ID = @id";
+                    cmd = new SqlCommand(updateQuery, conn);
                     using (cmd)
                     {
                         //cmd.Parameters.AddWithValue("@index", index);
                         cmd.Parameters.AddWithValue("@update", updatedValue);
                         cmd.Parameters.AddWithValue("@id", cookie["ID"]);
-                        cmd.ExecuteNonQuery();
+                        int c = cmd.ExecuteNonQuery();
                         cmd.Dispose();
+                        if (c > 0)
+                            MessageBox.Show("The item was updated successfully for: " + cookie["name"] + " " + cookie["surname"]);
+                        else
+                            MessageBox.Show("The update could not bve completed");
                     }
-                    sql = "Select * from [patients] WHERE [ID] LIKE '%" + cookie["ID"] +"'";
+                    //display the new information with the edited information
+                    sql = "Select * from [patients] WHERE [ID] LIKE '%" + cookie["ID"] + "'";
                     cmd = new SqlCommand(sql, conn);
                     dr = cmd.ExecuteReader();
                     GridView1.DataSource = dr;
                     GridView1.DataBind();
+                }
+                else
+                    MessageBox.Show("Please select a proper value/enter proper values");
             }
             catch (SqlException ex) 
             {
@@ -74,7 +89,7 @@ namespace WebApplication1
         public void establish()
         {
             
-
+            //method for displaying the patients relevant information after search
             if (cookie != null)
             {
                 lblDisplay.Text = "Showing the information of: " + cookie["name"] + " " + cookie["surname"];
